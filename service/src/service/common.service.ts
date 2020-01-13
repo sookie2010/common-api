66ac0102056873b2581fbddae01215475cde3fdb
结发受长生 <init_pro@aliyun.com>
2020-01-13 15:36:41 +0800

    token校验修改

--- service/src/service/common.service.ts
+++ service/src/service/common.service.ts
@@ -14,32 +14,23 @@ export default class CommonService {
   constructor(@InjectModel('SystemConfig') private readonly systemConfigModel: Model<SystemConfig>,
               @InjectModel('SystemUser') private readonly systemUserModel: Model<SystemUser>,
   ) {}
+
+  private tokenField = {_id: 1, username: 1, realname: 1, role_ids: 1}
   /**
    * 登录
    * @param systemUser 用户信息
    */
   async login(systemUser: SystemUser): Promise<object> {
     systemUser.password = CommonUtils.dataHash(systemUser.password, 'sha1')
-    let signUser = null
-    return this.systemUserModel.findOne(systemUser).exec().then((user: SystemUser) => {
-      if (!user) {
-        return Promise.reject({statusCode: 401, msg: '用户名/密码错误'})
-      }
-      signUser = {
-        _id: user._id.toString(),
-        username: user.username,
-        realname: user.realname,
-        role_ids: user.role_ids
-      }
-      return this.systemConfigModel.findOne({name: 'token_private_key'}).exec()
-    }).then((systemConfig: SystemConfig) => {
-      const token = jwt.sign(signUser, systemConfig.value.toString()/*秘钥*/, {
-        expiresIn: '7d' /*过期时间*/
-      })
-      return Promise.resolve({token, userInfo: signUser})
-    }).catch(result => {
-      return result
+    const signUser = await this.systemUserModel.findOne(systemUser, this.tokenField).exec()
+    if(!signUser) {
+      return Promise.reject({statusCode: 401, msg: '用户名/密码错误'})
+    }
+    const tokenKeyConfig: SystemConfig = await this.systemConfigModel.findOne({name: 'token_private_key'}).exec()
+    const token = jwt.sign(signUser, tokenKeyConfig.value.toString()/*秘钥*/, {
+      expiresIn: '7d' /*过期时间*/
     })
+    return Promise.resolve({token, userInfo: signUser})
   }
   /**
    * 校验Token
@@ -54,7 +45,8 @@ export default class CommonService {
       if (err instanceof jwt.TokenExpiredError) {
         // 如果token过期 则按照忽略过期时间再校验一次 并签发新的token
         const userInfo = jwt.verify(token, systemConfig.value.toString(), {ignoreExpiration: true})
-        const newToken = jwt.sign(userInfo, systemConfig.value.toString(), {expiresIn: '7d'})
+        const curUserInfo = await this.systemUserModel.findById(userInfo['_id'], this.tokenField).exec()
+        const newToken = jwt.sign(curUserInfo, systemConfig.value.toString(), {expiresIn: '7d'})
         return {status: true, userInfo, newToken}
       } else if (err instanceof jwt.JsonWebTokenError) {
         return new MsgResult(false, 'Token无效，请重新登录')
